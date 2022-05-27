e58660d5b1ca6f5564448a161b4e8b4929d8ad0a
lee <leehyunk6310@github.com>
2022-05-27 11:10:57 +0900

    ft: add two models for save mst, id, tst

---
+++ models/Mst_Id.go
@@ -0,0 +1,6 @@
+package models
+
+type Mst_Id struct {
+    MST          string				`json:"mst,omitempty" bson:"mst,omitempty"`
+    Id        	 string             `json:"id,omitempty" bson:"id,omitempty"`
+}
---
+++ models/Mst_Tst.go
@@ -0,0 +1,6 @@
+package models
+
+type Mst_Tst struct {
+    MST          string				`json:"mst,omitempty" bson:"mst,omitempty"`
+    TST        	 string             `json:"tst,omitempty" bson:"tst,omitempty"`
+}
